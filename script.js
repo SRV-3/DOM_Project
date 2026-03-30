075b00d07bf1a8ad44244eb208e0284f6cece35c
Sourav <thakursourav778@gmail.com>
2026-03-30 16:04:11 +0530

    Add back buttons to sections and improve styling; fix typo in Daily Planner heading

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const allElems = document.querySelectorAll(".elem");
 const allFullElems = document.querySelectorAll(".fullElem");
+const back = document.querySelectorAll(".back");
 
 allElems.forEach((elem) => {
   elem.addEventListener("click", () => {
     allFullElems[elem.id].style.display = "block";
+  });
+});
+
+back.forEach((elem) => {
+  elem.addEventListener("click", () => {
+    allFullElems[elem.id].style.display = "none";
   });
 });
