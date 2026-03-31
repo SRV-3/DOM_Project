bd72e6972f6555a230236c789cc239aed56eeff4
Sourav <thakursourav778@gmail.com>
2026-03-31 09:29:17 +0530

    Enhance todo list functionality and styling; update section headers and improve layout

--- script.js
+++ script.js
@@ -1,15 +1,74 @@
-const allElems = document.querySelectorAll(".elem");
-const allFullElems = document.querySelectorAll(".fullElem");
-const back = document.querySelectorAll(".back");
+function openFeatures() {
+  const allElems = document.querySelectorAll(".elem");
+  const allFullElems = document.querySelectorAll(".fullElem");
+  const allBackButtons = document.querySelectorAll(".back");
 
-allElems.forEach((elem) => {
-  elem.addEventListener("click", () => {
-    allFullElems[elem.id].style.display = "block";
+  allElems.forEach((elem) => {
+    elem.addEventListener("click", () => {
+      allFullElems[elem.id].style.display = "block";
+    });
   });
-});
 
-back.forEach((elem) => {
-  elem.addEventListener("click", () => {
-    allFullElems[elem.id].style.display = "none";
+  allBackButtons.forEach((elem) => {
+    elem.addEventListener("click", () => {
+      allFullElems[elem.id].style.display = "none";
+    });
   });
-});
+}
+openFeatures();
+
+function todoList() {
+  const form = document.querySelector(".addTask form");
+  const taskInput = document.querySelector(".addTask form #task");
+  const taskDetailsInput = document.querySelector(".addTask textarea");
+  const taskCheck = document.querySelector(".addTask form #check");
+
+  let currentTask = [];
+
+  if (localStorage.getItem("currentTask")) {
+    currentTask = JSON.parse(localStorage.getItem("currentTask"));
+  } else {
+    console.log("task list is empty");
+  }
+
+  function renderTask() {
+    const allTAsk = document.querySelector(".allTask");
+    let task = "";
+    currentTask.forEach((elem, idx) => {
+      task =
+        task +
+        `<div class="task">
+              <h5>${elem.task}<span class="${elem.imp}">imp</span></h5>
+              <button id="${idx}">Mark as done</button>
+            </div>`;
+    });
+    allTAsk.innerHTML = task;
+    localStorage.setItem("currentTask", JSON.stringify(currentTask));
+  }
+  renderTask();
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    currentTask.push({
+      task: taskInput.value,
+      details: taskDetailsInput.value,
+      imp: taskCheck.checked,
+    });
+
+    renderTask();
+
+    location.reload();
+  });
+
+  const markCompletedBtn = document.querySelectorAll(".task button");
+
+  markCompletedBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      currentTask.splice(btn.id, 1);
+      renderTask();
+      location.reload();
+    });
+  });
+}
+
+todoList();
